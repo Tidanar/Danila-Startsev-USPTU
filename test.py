8eca783882c7681d39bab81afbd5da901a6c702f
Данила <tidanar@yandex.ru>
2021-06-11 01:24:24 +0500

    interpolate

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print('hello')
+from scipy import interpolate
+
+
+def f(x_in):
+    x = [0, 1, 2, 3, 4, 5]
+    y = list(map(lambda a: a ** 3 + 7, x))
+    return interpolate.splev(x_in, interpolate.splrep(x, y))
+
+
+x = list(map(lambda a: 4 + a * 3 / 1000, range(1000)))
+print(f(x))
